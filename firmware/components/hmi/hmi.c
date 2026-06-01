5790adc048e8bbd34fe08523cd3e5bd43da7fd1d
João Pedro Maciel Freitas <joaopedromacielfreitas@outlook.com>
2026-06-01 20:40:51 -0300

    [test] board led testing

--- firmware/components/hmi/hmi.c
+++ firmware/components/hmi/hmi.c
@@ -6,22 +6,42 @@
 #include "hmi.h"
 #include "app_context.h"
 #include "esp_log.h"
+#include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
+#include "driver/gpio.h"
+#include "hal/gpio_types.h"
 
 static const char *TAG = "hmi";
+
+#define LED_GPIO 48
 
 void task_hmi(void *arg)
 {
     app_context_t *ctx = (app_context_t *)arg;
     (void)ctx;  // Suprime warning
     ESP_LOGI(TAG, "task_hmi iniciada no Core 1");
-    
+
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << LED_GPIO),
+        .mode = GPIO_MODE_INPUT_OUTPUT, // IMPORTANTE: permite ler e escrever
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&io_conf);
+
+     bool led_ligado = false;
+
     while (1) {
         // TODO: Implementar HMI:
         // 1. Ler dados de queue_dsp_result
         // 2. Atualizar display com valores
         // 3. Processar entrada de botões
         // 4. Controlar LEDs de status
-        
-        vTaskDelay(pdMS_TO_TICKS(100));
+        led_ligado = !led_ligado; // Inverte a variável
+        gpio_set_level(LED_GPIO, led_ligado);
+        ESP_LOGD(TAG, "Alternando estado do led");
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
