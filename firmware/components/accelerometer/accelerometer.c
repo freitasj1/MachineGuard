284987e3eb909c5807f976d70fb405a462229fea
João Pedro Maciel Freitas <joaopedromacielfreitas@outlook.com>
2026-07-09 22:48:58 -0300

    feat(main && accel): SPI init and Mutex SPI2 implemented

--- firmware/components/accelerometer/accelerometer.c
+++ firmware/components/accelerometer/accelerometer.c
@@ -4,25 +4,113 @@
  */
 
 #include "accelerometer.h"
+#include "app_context.h"
 #include "esp_log.h"
 #include "esp_err.h"
+#include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
+#include "portmacro.h"
+#include <stddef.h>
+#include <stdint.h>
+
+#include "driver/spi_master.h"
+
+/* ============================================================================
+* Private constants and macros
+* ========================================================================== */
+
+#define ACCEL_BUFFER_SIZE 2048
 
 static const char *TAG = "accelerometer";
 
-esp_err_t accel_init(SemaphoreHandle_t spi2_mutex)
+
+/* ============================================================================
+ * Private types
+ * ========================================================================== */
+/**
+ * @brief Buffer duplo de aquisição.
+ *
+ * O driver escreve continuamente em um buffer enquanto o outro
+ * permanece disponível para processamento pelo DSP.
+ *
+ * Esta estrutura é privada do módulo accelerometer.
+ */
+typedef struct
 {
-    (void)spi2_mutex;
-    ESP_LOGI(TAG, "accel_init chamado");
-    // TODO: inicializar acelerômetro e SPI2
+    accel_sample_t ping[ACCEL_BUFFER_SIZE];
+    accel_sample_t pong[ACCEL_BUFFER_SIZE];
+
+    accel_sample_t *write;
+    accel_sample_t *process;
+
+    uint16_t index;
+
+    bool ping_active;
+
+} accel_stream_t;
+
+/* ============================================================================
+ * Public variables
+ * ========================================================================== */
+
+
+
+/* ============================================================================
+ * Private variables
+ * ========================================================================== */
+
+static spi_device_handle_t accel_spi;
+
+static app_context_t *ctx;
+
+/* ============================================================================
+ * Private function prototypes
+ * ========================================================================== */
+
+static esp_err_t sensor_reset(void);
+
+static esp_err_t sensor_config(void);
+static esp_err_t read_register(uint8_t reg, uint8_t *data, size_t len);
+
+static esp_err_t write_register(uint8_t reg, const uint8_t *data, size_t len);
+
+
+/* ============================================================================
+ * Public function implementations
+ * ========================================================================== */
+
+esp_err_t accel_init(app_context_t *context)
+{
+    if (context == NULL)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+    
+    ctx = context;
+
+   
     return ESP_OK;
+}
+void task_accel(void *arg)
+{
+    esp_err_t err_accel;
+    err_accel = accel_init((app_context_t *)arg);
+    if(err_accel != ESP_OK) ESP_LOGE(TAG, "accelerometer init failed, err code = %u", err_accel);
+
+
+    while (1)
+    {
+        if (xSemaphoreTake(ctx->mutex_spi2,
+                           portMAX_DELAY) == pdTRUE)
+        {
+            /* Futuramente SPI */
+
+            xSemaphoreGive(ctx->mutex_spi2);
+        }
+    }
 }
 
-esp_err_t accel_read_raw(accel_sample_t *out)
-{
-    if (out == NULL) return ESP_ERR_INVALID_ARG;
-    out->x = 0;
-    out->y = 0;
-    out->z = 0;
-    // TODO: ler dados reais do acelerômetro
-    return ESP_OK;
-}
+
+/* ============================================================================
+ * Private function implementations
+ * ========================================================================== */
--- firmware/components/accelerometer/include/accelerometer.h
+++ firmware/components/accelerometer/include/accelerometer.h
@@ -15,8 +15,10 @@ extern "C" {
 
 #define ACCEL_MG_PER_LSB 0.061f
 
-esp_err_t accel_init(SemaphoreHandle_t spi2_mutex);
-esp_err_t accel_read_raw(accel_sample_t *out);
+esp_err_t accel_init(app_context_t *ctx);
+void task_accel(void *arg);
+
+
 
 #ifdef __cplusplus
 }
